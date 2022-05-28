23ac5ee183952a41b9fcd644b292ddb1577831a2
Nitzan Cohen <nitzancohen@hotmail.com>
2022-05-28 15:25:43 +0300

    Added celebrate validation to user routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -14,16 +14,20 @@ const {
 
 // Every path begin with: /users/...
 router.get('/', getUsers);
+
 router.get('/me', getCurrentUser);
+
 router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
   }),
 }), updateProfile);
+
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string(),
+    avatar: Joi.string()
+    .pattern(new RegExp('/^http(s)?:\/{2}(w{3}.)?[\w-]+.\w+/ig')),
   }),
 }), updateProfileAvatar);
 
