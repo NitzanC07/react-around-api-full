73018433617e37da61f5aa707735eb5a7eca34d9
Nitzan Cohen <nitzancohen@hotmail.com>
2022-05-28 17:14:25 +0300

    start implementing error handler.

--- backend/helpers/error.js
+++ backend/helpers/error.js
@@ -5,26 +5,24 @@ class ErrorHandler extends Error {
     this.message = message;
   }
 
-}
+  handleAnyError = (err, res) => {
+    const { statusCode, message } = err;
+    res.status(statusCode).json({
+      status: "error",
+      statusCode,
+      message
+    });
+  };
 
-const handleAnyError = (err, res) => {
-  console.log(`ErrorHandler: ${err}`);
-  const { statusCode, message } = err;
-  res.status(statusCode).json({
-    status: "error",
-    statusCode,
-    message
-  });
-};
-
-const handleServerError = (err, res) => {
-  console.log(`ErrorHandler: ${err}`);
-  res.status(500).json({
-    error: {message: 'Somthing went wrong with the server.'}
-  })
+  handleServerError = (err, res) => {
+    res.status(500).json({
+      error: {message: 'Somthing went wrong with the server.'}
+    })
+  }
 }
 
 const customErrorHandler = (err, res) => {
+  console.log(`ErrorHandler: ${err}`);
   err instanceof ErrorHandler
     ? handleAnyError(err, res)
     : handleServerError(err, res);
